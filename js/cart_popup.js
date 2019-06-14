e0a57430b497ef9887063f373c87ba5794868fc5
Dmitry Kuchukov <kudim84@yandex.ru>
2019-06-14 05:50:22 +0300

    customize checkbox

--- js/cart_popup.js
+++ js/cart_popup.js
@@ -1,23 +1,25 @@
-var map = document.querySelector(".open_map");
-var modalMap = document.querySelector(".map_popup");
-var close = document.querySelector(".map_popup .popup_close");
+var cart = document.querySelector(".goods_btn_cart");
+var modalCart = document.querySelector(".cart_popup");
+var close = document.querySelector(".cart_popup .popup_close");
 
 
-map.addEventListener("click", function (evt) {
+
+
+cart.addEventListener("click", function (evt) {
   evt.preventDefault();
-  modalMap.classList.add("map_popup_show");
+  modalCart.classList.add("cart_popup_show");
 });
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
-  modalMap.classList.remove("map_popup_show");
+  modalCart.classList.remove("cart_popup_show");
 });
 
 window.addEventListener("keydown", function (evt) {
   if(evt.keyCode === 27) {
-    if(modalMap.classList.contains("map_popup_show")){
+    if(modalCart.classList.contains("cart_popup_show")){
       evt.preventDefault();
-      modalMap.classList.remove("map_popup_show");
+      modalCart.classList.remove("cart_popup_show");
     }
   }
 });
